e9b444faed63a149a06349d83dc939786c3ac5f7
duch3321 <duch3321@gmail.com>
2026-03-17 00:46:14 +0600

    Updated Student.java

--- src/Student.java
+++ src/Student.java
@@ -6,6 +6,9 @@ public class Student implements Comparable<Student> {
     // TODO: Task 1 — Создай конструктор, который принимает имя, GPA и возраст
     public Student(String name, double gpa, int age) {
         // заглушка
+        this.name=name;
+        this.gpa=gpa;
+        this.age=age;
     }
 
     public String getName() { return name; }
@@ -15,18 +18,23 @@ public class Student implements Comparable<Student> {
     // TODO: Task 1 — Добавь возможность изменить GPA
     public void setGpa(double newGpa) {
         // заглушка
+        if(newGpa<=4 && newGpa>0){
+            this.gpa=newGpa;
+        }
     }
 
     // TODO: Task 1 (Sorting) — Реализуй Comparable по GPA (по возрастанию)
     @Override
     public int compareTo(Student other) {
         // заглушка — верни правильное значение сравнения
-        return 0;
+        return Double.compare(this.gpa,other.gpa);
     }
 
     @Override
     public String toString() {
         // TODO: Сделай красивый вывод
-        return "";
+        return "Name : "+this.name
+                +" , Age : "+this.age
+                +" , GPA : "+this.gpa;
     }
 }
